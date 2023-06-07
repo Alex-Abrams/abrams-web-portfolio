489cb42a3f7cad026d01f13b19cff28b8a1aba92
alex-abrams <alexander.abrams99@gmail.com>
2023-06-07 16:39:31 -0700

    Added seperate ribbon component.

--- components/navbars/resume_ribbon.js
+++ components/navbars/resume_ribbon.js
@@ -1,4 +1,10 @@
 import styles from "./resume_ribbon.module.css";
 export default function ResumeRibbon() {
-  return <div className={styles.ribbonContainer}>RIBBON</div>;
+  // return <div className={styles.ribbonContainer}>RIBBON</div>;
+  return(
+
+    <div className={styles.ribbon}>
+      <span>POPULAR</span>
+    </div>
+  );
 }
