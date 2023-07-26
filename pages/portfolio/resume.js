115a996d847eaa67a754df3e81bf14f6583fd974
alex-abrams <alexander.abrams99@gmail.com>
2023-07-26 16:13:41 -0700

    Removed stickifiy npm. Reformated Resume experience.

--- pages/portfolio/resume.js
+++ pages/portfolio/resume.js
@@ -22,6 +22,7 @@ export default function Resume() {
 
         <h1 className={styles.header}>Profesional Skills and Experience</h1>
 
+      <div className={styles.resumeContent}>
         <div className={styles.aboutMe}>
         <h2>About Me</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -36,65 +37,34 @@ export default function Resume() {
         <div className={styles.uiuxTechs}>
           <h2>UI/UX Technologies</h2>
           <ul>
-            <li>figma</li>
-            <li>figma</li>
-            <li>figma</li>
+            <p>Figma, Adobe XD, Sketch</p>
           </ul>
         </div>
 
         <div className={styles.workExp}>
           <h2>Professional Experience</h2>
           <ul>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
-            <li>JavaScript</li>
+            <h3>Revature LLC.<span>10/1/1022 - 10/1/1022</span></h3>
+            <div>Junior Developer</div>
+            <li>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </li>
+
+            <h3>Frelance Web Developer<span>10/1/1022 - 10/1/1022</span></h3>
+            <div>Junior Developer</div>
+            <li>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </li>
+
+            <h3>Crawford Pimentel & Co<span>10/1/1022 - 10/1/1022</span></h3>
+            <div>Junior Developer</div>
+            <li>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </li>
           </ul>
         </div>
 
-        <h2>Professional Experience</h2>
-        <ul>
-          <li>Revature</li>
-          <li>CPCO</li>
-        </ul>
+        </div>
       </div>
     </>
   );
