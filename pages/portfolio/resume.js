37be2960e1ce7aa8cb8cc1049cf78573100a973b
alex-abrams <alexander.abrams99@gmail.com>
2023-07-26 18:20:07 -0700

    Minor fixes to styling on resume page.

--- pages/portfolio/resume.js
+++ pages/portfolio/resume.js
@@ -44,20 +44,23 @@ export default function Resume() {
         <div className={styles.workExp}>
           <h2>Professional Experience</h2>
           <ul>
+          <div>
+
+          </div>
             <h3>Revature LLC.<span>10/1/1022 - 10/1/1022</span></h3>
-            <div>Junior Developer</div>
+            <jobtitle>Junior Developer</jobtitle>
             <li>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </li>
 
             <h3>Frelance Web Developer<span>10/1/1022 - 10/1/1022</span></h3>
-            <div>Junior Developer</div>
+            <jobtitle>Junior Developer</jobtitle>
             <li>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </li>
 
             <h3>Crawford Pimentel & Co<span>10/1/1022 - 10/1/1022</span></h3>
-            <div>Junior Developer</div>
+            <jobtitle>Junior Developer</jobtitle>
             <li>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </li>
