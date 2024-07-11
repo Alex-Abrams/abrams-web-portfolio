c55990fe925f26fc3680cb511222c2edecf8c91a
alex-abrams <alexander.abrams99@gmail.com>
2024-07-11 11:45:12 -0700

    Added layout for ui boxes and project display.

--- pages/portfolio/ui_ux.js
+++ pages/portfolio/ui_ux.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import styles from './uiUX.module.css';
+
 export default function UiUx() {
+  const SquareLayout = () => {
+    return (
+      <div className={styles.container}>
+        <div className={styles.squareTop}></div>
+        <div className={styles.middleContainer}>
+          <div className={styles.squareMiddle}></div>
+          <div className={styles.squareMiddle}></div>
+          
+        </div>
+        <div className={styles.squareBottom}></div>
+      </div>
+    );
+  };
+
   return (
-    <>
-      <p>
-        here will be all my ui and ux links or something to portfoilios as well
-        as a slide show or something or whatever
-      </p>
-    </>
+    <div>
+      <SquareLayout />
+    </div>
   );
 }
+
+
+
